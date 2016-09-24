0f2ca08fb4c111dcf916a3529e4c22cf535d113c
Sergey Smirnov <s@tigr.bz>
2016-09-25 01:16:42 +0300

    remove click counter.

--- js/app.js
+++ js/app.js
@@ -68,16 +68,6 @@ var News = React.createClass({
         data: React.PropTypes.array.isRequired
     },
 
-    getInitialState: function () {
-        return {
-            counter: 0
-        }
-    },
-
-    onTotalNewsClick: function (e) {
-        this.setState({counter: ++this.state.counter})
-    },
-
     render: function() {
         var data = this.props.data;
         var newsTemplate;
@@ -96,8 +86,7 @@ var News = React.createClass({
         return (
             <div className="news">
                 {newsTemplate}
-                <strong className={'news__count ' + (data.length > 0 ? '' : 'none')}
-                    onClick={this.onTotalNewsClick}>
+                <strong className={'news__count ' + (data.length > 0 ? '' : 'none')}>
                     Всего новостей: {data.length}</strong>
             </div>
         );
