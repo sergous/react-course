1d5ff7dabcaee9d710e8e1e58395ea806f33ce4b
Sergey Smirnov <s@tigr.bz>
2016-09-24 21:18:52 +0300

    add news components.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,36 @@
+var App = React.createClass({
+    render: function() {
+        return (
+            <div className="app">
+                Всем привет, я компонент App!
+                <News />
+            </div>
+        );
+    }
+});
+
+var News = React.createClass({
+    render: function() {
+        return (
+            <div className="news">
+                К сожалению, новостей нет.
+                <Comments />
+            </div>
+        );
+    }
+});
+
+var Comments = React.createClass({
+    render: function () {
+        return (
+            <div className="comments">
+                Нет новостей - комментировать нечего.
+            </div>
+        )
+    }
+});
+
 ReactDOM.render(
-    <h1>Привет, Мир!</h1>,
+    <App />,
     document.getElementById('root')
 );
