9b3f4b6f78ac24bdffecd61030112573cc1cc423
Sergey Smirnov <s@tigr.bz>
2016-09-25 02:20:03 +0300

    add form validation.

--- js/app.js
+++ js/app.js
@@ -16,27 +16,76 @@ var my_news = [
     }
 ];
 
-var TestInput = React.createClass({
-    componentDidMount: function () {
-        ReactDOM.findDOMNode(this.refs.myTestInput).focus();
+var Add = React.createClass({
+    getInitialState: function() {       //устанавливаем начальное состояние (state)
+        return {
+            btnIsDisabled: true,
+            agreeNotChecked: true,
+            authorIsEmpty: true,
+            textIsEmpty: true
+        };
     },
-
-    onSubmit: function (e) {
-        console.log(this.refs);
-        alert(ReactDOM.findDOMNode(this.refs.myTestInput).value)
+    componentDidMount: function() {
+        ReactDOM.findDOMNode(this.refs.author).focus();
     },
-
-    render: function () {
+    onBtnClickHandler: function(e) {
+        e.preventDefault();
+        var author = ReactDOM.findDOMNode(this.refs.author).value;
+        var text = ReactDOM.findDOMNode(this.refs.text).value;
+        alert(author + '\n' + text);
+    },
+    onCheckRuleClick: function(e) {     //устанавливаем значение в state
+        this.setState({
+            agreeNotChecked: !this.state.agreeNotChecked
+        });
+    },
+    onAuthorChange: function(e) {
+        if (e.target.value.trim().length > 0) {
+            this.setState({authorIsEmpty: false})
+        } else {
+            this.setState({authorIsEmpty: true})
+        }
+    },
+    onTextChange: function(e) {
+        if (e.target.value.trim().length > 0) {
+            this.setState({textIsEmpty: false})
+        } else {
+            this.setState({textIsEmpty: true})
+        }
+    },
+    render: function() {
+        var agreeNotChecked = this.state.agreeNotChecked,
+            authorIsEmpty = this.state.authorIsEmpty,
+            textIsEmpty = this.state.textIsEmpty;
         return (
-            <div>
-                <input className="test-input"
-                       defaultValue=''
-                       ref="myTestInput"
-                       placeholder='введите значение' />
-
-                <button type='submit' onClick={this.onSubmit}>Добавить</button>
-            </div>
-        )
+            <form className='add cf'>
+                <input
+                    type='text'
+                    className='add__author'
+                    placeholder='Ваше имя'
+                    onChange={this.onAuthorChange}
+                    ref='author'
+                />
+                <textarea
+                    className='add__text'
+                    placeholder='Текст новости'
+                    onChange={this.onTextChange}
+                    ref='text'>
+                </textarea>
+                <label className='add__checkrule'>
+                    <input type='checkbox'
+                           onChange={this.onCheckRuleClick}
+                           ref='checkrule' />Я согласен с правилами
+                </label>
+                <button
+                    className='add__btn'
+                    disabled={agreeNotChecked || authorIsEmpty || textIsEmpty}
+                    onClick={this.onBtnClickHandler}
+                    ref='alert_button'>
+                    Показать alert
+                </button>
+            </form>
+        );
     }
 });
 
@@ -45,7 +94,7 @@ var App = React.createClass({
         return (
             <div className="app">
                 <h3>Новости</h3>
-                <TestInput />
+                <Add />
                 <News data={my_news} />
             </div>
         );
