f8c604dff6b042040c3f37417b4b06551feb4598
Sergey Smirnov <s@tigr.bz>
2016-09-24 21:32:56 +0300

    list news.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
+var my_news = [
+    {
+        author: 'Саша Печкин',
+        text: 'В четверг, четвертого числа...'
+    },
+    {
+        author: 'Просто Вася',
+        text: 'Считаю, что $ должен стоить 35 рублей!'
+    },
+    {
+        author: 'Гость',
+        text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+    }
+];
+
 var App = React.createClass({
     render: function() {
         return (
             <div className="app">
-                Всем привет, я компонент App!
-                <News />
+                Всем привет, я компонент App! Я умею отображат новости.
+                <News  data={my_news} />
             </div>
         );
     }
@@ -11,9 +26,19 @@ var App = React.createClass({
 
 var News = React.createClass({
     render: function() {
+        var data = this.props.data;
+        var newsTemplate = data.map(function (item, index) {
+            return (
+                <div key={index}>
+                    <p className="news_author">{item.author}:</p>
+                    <p className="news_text">{item.text}</p>
+                </div>
+            )
+        });
+
         return (
             <div className="news">
-                К сожалению, новостей нет.
+                {newsTemplate}
                 <Comments />
             </div>
         );
